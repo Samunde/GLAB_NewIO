0491a544baf010208a036db8d880edf40ad0a75e
Samundeeswari Velusamy <sam.velusamy36@gmail.com>
2024-05-06 18:19:39 -0400

    Guided Lab - 303.13.2 - New I/O

---
+++ src/com/sam/Nio/Main.java
@@ -0,0 +1,17 @@
+package com.sam.Nio;
+
+import java.io.IOException;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+//        com.sam.Nio.NioExample obj = new com.sam.Nio.NioExample();
+//        obj.nioPractice();
+
+
+        NioExampleTwo obj2 = new NioExampleTwo();
+        obj2.nioPracticeTwo();
+    }
+}
